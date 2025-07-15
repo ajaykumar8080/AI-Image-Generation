20bf1405a282f6638f67d5a7fb18e209e9866fdb
G AJAY KUMAR <luckyajay8088@gmail.com>
2025-07-15 12:18:04 +0000

    remove both small bg buttons make it to transpranent colors add for symb

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -14,7 +14,7 @@ export function Header() {
         <div className="flex flex-1 items-center justify-end space-x-2">
           <nav className="flex items-center space-x-1">
             <ThemeToggle />
-            <Button variant="outline">Sign In</Button>
+            <Button variant="ghost">Sign In</Button>
           </nav>
         </div>
       </div>
